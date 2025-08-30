7330dffe1e4f1b6e24dce2b61f77feab7c676e08
Jianping <lijianping@whu.edu.cn>
2025-08-30 13:43:56 +0800

    add first

---
+++ scripts/init_pose_pub.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import PoseStamped
+from fast_lio.msg import RelocalizationMsg
+from tf.transformations import quaternion_from_euler
+
+rospy.init_node('localization_init_pose_pub')
+pub = rospy.Publisher('/relocalization', RelocalizationMsg, queue_size=1, latch=True)
+
+# Create RelocalizationMsg
+relocalization_msg = RelocalizationMsg()
+
+# Set PCD path
+relocalization_msg.pcd_path = "/home/amov/PersonalData/Program/Ros/dispatch_ws/src/dispatch_control/Map/ntuitive/4/4.pcd"
+
+# Set initial pose
+relocalization_msg.init_pose = PoseStamped()
+relocalization_msg.init_pose.header.frame_id = "map"
+relocalization_msg.init_pose.header.stamp = rospy.Time.now()
+
+relocalization_msg.init_pose.pose.position.x = 2.6
+relocalization_msg.init_pose.pose.position.y = -1.2
+relocalization_msg.init_pose.pose.position.z = 0
+
+roll = 0.0
+pitch = 0.0
+yaw = -1.83
+
+quaternion = quaternion_from_euler(roll, pitch, yaw)
+relocalization_msg.init_pose.pose.orientation.x = quaternion[0]
+relocalization_msg.init_pose.pose.orientation.y = quaternion[1]
+relocalization_msg.init_pose.pose.orientation.z = quaternion[2]
+relocalization_msg.init_pose.pose.orientation.w = quaternion[3]
+
+pub.publish(relocalization_msg)
+rospy.sleep(1.0)
